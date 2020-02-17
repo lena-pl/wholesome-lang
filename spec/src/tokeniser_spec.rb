71804b4810a11e8a16bd43bc7d09d88d64e508a0
Lena Plaksina <lena.plaksina@gmail.com>
2020-02-17 22:27:18 +1300

    Update tokeniser_spec to reflect current method signature and code paths

--- spec/src/tokeniser_spec.rb
+++ spec/src/tokeniser_spec.rb
@@ -7,13 +7,43 @@ describe Tokeniser do
     EOS
   end
 
-  subject { Tokeniser.new(program) }
+  subject { Tokeniser.new(program, dictionary) }
+
+  let(:dictionary) { [] }
   let(:parse_result) { subject.call }
 
-  it "successfully tokenises the program" do
-    expected_result = [IdentifierToken.new("psst"), LiteralToken.new("Hello, from Wholesomelang")]
+  RSpec.shared_examples "the happy case" do
+    it "successfully tokenises the program" do
+      expected_result = [IdentifierToken.new("psst"), LiteralToken.new("Hello, from Wholesomelang")]
 
-    expect(parse_result.map(&:class)).to eq expected_result.map(&:class)
-    expect(parse_result.map(&:value)).to eq expected_result.map(&:value)
+      expect(parse_result.map(&:class)).to eq expected_result.map(&:class)
+      expect(parse_result.map(&:value)).to eq expected_result.map(&:value)
+    end
+  end
+
+  context "with an empty dictionary" do
+    it_behaves_like "the happy case"
+  end
+
+  context "with a populated dictionary" do
+    let(:rude_word) { 'impolite' }
+
+    before { dictionary << rude_word }
+
+    context "with polite source code" do
+      it_behaves_like "the happy case"
+    end
+
+    context "with a rude word in the source code" do
+      let(:program) do
+        <<~EOS
+          psst #{rude_word}
+        EOS
+      end
+
+      it "raises a critical rude" do
+        expect{ parse_result }.to raise_error(RuntimeError, /CRITICAL RUDE/)
+      end
+    end
   end
 end
