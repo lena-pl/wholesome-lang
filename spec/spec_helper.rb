fb988dae5977b315acdf169e7b0ddb3e1740a347
Lena Plaksina <lena.plaksina@gmail.com>
2020-02-17 22:26:34 +1300

    Load required components via component file in the spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,8 +14,13 @@
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
-  require_relative "../wholesome-lang.rb"
-  require "byebug"
+  $LOAD_PATH << '../src'
+  require './components'
+
+  require 'byebug'
+
+  config.color = true
+  config.formatter = :documentation
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
